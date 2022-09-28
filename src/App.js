88565522cbbc4ba37c53b66761f69762335703e0
Olivia Adongo <akinyioliviatove@gmail.com>
2022-09-28 19:26:34 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,111 +1,172 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import Form from './components/Form';
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return(
-    <div>
-      <Form/>
+  const [attendee, setAttendee] = useState({});
+
+  const [attendees, setAttendees] = useState([]);
+
+  function handleChange(e) {
+    setAttendee({...attendee,[e.target.name]: e.target.value});
+  }
+
+  // handle create & update
+  const handleFormSubmission = async (event, func) => {
+    event.preventDefault();
+
+    const URL = func === 'create'
+      ? `http://localhost:3001/users`
+      : `http://localhost:3001/${attendee?.id}`;
+
+    const method = func === 'create' ? "POST" : "PUT";
+
+    try {
+      const resp = await fetch(URL, {method, headers: { "Content-Type": "application/json" }, body: JSON.stringify(attendee)});
+
+      if (resp.status === 200) setAttendee({});
+
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const resp = await fetch("http://localhost:3001/users");
+
+      const json = await resp.json();
+
+      if (resp.status === 200) setAttendees(json)
+      } catch (error) {
+        setAttendees([])
+      }
+
+    })();
+  }, []);
+
+  function deleteList(id) {
+    fetch(`http://localhost:3001/users/${id}`, {
+      method: "DELETE",
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        
+      });
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-5 col-lg-6">
+          Form
+          <form method="POST">
+            <label class="form-label">Full Name</label>
+            <input
+              name="name"
+              type="text"
+              value={attendee.name}
+              class="form-control"
+              placeholder="Full Name"
+              onChange={handleChange}
+            />
+            <label class="form-label">Email address</label>
+            <input
+              name="email"
+              type="email"
+              value={attendee.email}
+              class="form-control"
+              placeholder="Email Address"
+              onChange={handleChange}
+            />
+            <label class="form-label">Age</label>
+            <input
+              name="age"
+              type="number"
+              value={attendee.age}
+              class="form-control"
+              placeholder="Age"
+              onChange={handleChange}
+            />
+            <label class="form-label">Mobile</label>
+            <input
+              name="mobile"
+              type="tel"
+              value={attendee.mobile}
+              class="form-control"
+              placeholder="Mobile Number"
+              onChange={handleChange}
+            />
+            <p className="mt-2">Gender</p>
+            <div class="form-check">
+              <input
+                name="gender"
+                class="form-check-input"
+                type="radio"
+                value={attendee.gender}
+                id="flexRadioDefault1"
+                onChange={handleChange}
+              />
+              <label class="form-check-label" for="flexRadioDefault1">
+                Male
+              </label>
+            </div>
+            <div class="form-check">
+              <input
+                name="gender"
+                class="form-check-input mt-2"
+                type="radio"
+                id="flexRadioDefault2"
+                onChange={handleChange}
+              />
+              <label class="form-check-label" for="flexRadioDefault2">
+                Female{" "}
+              </label>
+            </div>
+            <button
+              type="submit"
+              class="btn btn-primary mt-2"
+              onClick={(event) => handleFormSubmission(event, attendee?.id ? 'update': 'create')}
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+
+        <div className="col-12 col-md-7 col-lg-6">
+          List
+          <div>
+            {
+              (Array.isArray(attendees) ? attendees : []).map((person) => {
+      
+              return (
+                <div>
+                  <ul>
+                    <li>
+                      Full Name: {person.name}
+                      <br />
+                      Email Address: {person.email}
+                      <br />
+                      Mobile Number: {person.mobile}
+                      <br />
+                      Gender: {person.gender}
+                    </li>
+                    <button onClick={() => setAttendee(person)} className="btn btn-primary">Edit</button>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => deleteList(person.id)}
+                    >
+                      {" "}
+                      Delete
+                    </button>
+                  </ul>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
-  )
-  //state declaration
-  // const [attendees, setAttendees] = useState([]);
-  // const [formData, setFormData] = useState({
-
-  //   fullName: '',
-  //   emailAddress: '',
-  //   age: '',
-  //   mobileNumber: '',
-  //   gender:''
-
-  // });
-
-  // //get all event-goers
-  // const fetchData = () => {
-  //   fetch("http://localhost:4000/users")
-  //   .then(res => res.json())
-  //   .then(data => {setAttendees(data)})
-  // }
-
-	// function deleteList(id) {
-  //   fetch(`http://localhost:4000/users/${id}`, {
-  //     method: "DELETE",
-  //   })
-  //     .then((r) => r.json())
-  //     .then(() => {
-  //       fetchData()
-  //     });
-  // }
-  // // const fetchData = async () => {
-  // //   const response = await fetch("https://localhost:4000/users")
-  // //   const data = await response.json()
-  // //   setAttendees(data)
-  // // }
-
-  // useEffect(() => {
-  //   fetchData()
-  // }, []);
-
-  // //create an event-goer
-  // function handleChange(event) {
-  //   setFormData({
-  //     ...formData,
-  //     [event.target.name]: event.target.value,
-  //   });
-  // }
-
-  // //remove an event-goer
-
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   fetch("http://localhost:4000/users", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json"
-  //     }, 
-  //     body: JSON.stringify(
-	// 			formData
-  //     )
-  //   })
-
-  // }
-  // return (
-  //   <div className="container">
-  //     <div className='row'>
-  //       <div className='col-12 col-md-5 col-lg-5'>
-  //         <h1>Register for the Event</h1>
-  //         <form onSubmit={handleSubmit}>
-  //           <label className="form-label">Full Name</label>
-  //           <input type="text" class="form-control" name='fullName' value={formData.fullName} placeholder="Full Name" onChange={handleChange}/>
-
-  //           <label className="form-label">Email Address</label>
-  //           <input type="text" class="form-control" name='emailAddress' value={formData.emailAddress} placeholder="name@example.com" onChange={handleChange}/>
-
-  //           <label className="form-label">Age</label>
-  //           <input type="text" class="form-control" name='age' value={formData.age} placeholder="age" onChange={handleChange}/>
-
-  //           <label className="form-label">Mobile Number</label>
-  //           <input type="text" class="form-control" name='mobileNumber' value={formData.mobileNumber} placeholder="mobile number 07..." onChange={handleChange}/>
-
-  //           <label className="form-label">Gender</label>
-  //           <input type="text" class="form-control" name='gender' value={formData.gender} placeholder="gender" onChange={handleChange} />
-
-  //           <button className="btn btn-primary" type="submit">Submit Registration</button>
-  //         </form>
-          
-  //       </div>
-  //       <div className='col-12 col-md-5 col-lg-7'>
-  //         <h1>List of confirmed attendees</h1>
-  //         <ul>
-  //           {attendees.map( attendee => (
-  //             <li key={attendee.id}>{attendee.fullName} {attendee.mobileNumber} <button className='btn btn-danger' onClick={() => deleteList(attendee.id)}>Delete</button></li>
-  //           ))}
-  //         </ul>
-          
-  //       </div>
-  //     </div>
-  //   </div>
-  // );
+  );
 }
 
 export default App;
